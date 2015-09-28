9adc3bdc7e4ab304f6aa5bc17e2d1d21a2acf651
beavermosteger <brian.spargur@gmail.com>
2015-09-27 22:38:57 -0400

    Create update_baseline.sh

---
+++ automation_scripts/update_baseline.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+# Copyright (C) 2015 Brian Spargur
+# Copyright (C) 2015 Kevin Spargur
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License Version 2 as
+# published by the Free Software Foundation.  You may not use, modify or
+# distribute this program under any other version of the GNU General
+# Public License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+ROOTPATH="/home/beaver/projects/eagerbeaver"
+CURPATH=`pwd`
+MASTERSUM="./2word_metrics/master_summaries/master_summary"
+
+if /usr/sbin/pidof -x $(basename $0) > /dev/null; then
+  for p in $(/usr/sbin/pidof -x $(basename $0)); do
+    if [ $p -ne $$ ]; then
+      echo "Script $0 is already running: exiting"
+      exit
+    fi
+  done
+fi
+
+
+cd $ROOTPATH;
+
+./3baseline.pl -m $MASTERSUM
+
+cd $CURPATH;
+
+
